85d51f712b6bcb841c7baad5b5ec97fd1ec0fb6f
wojciechkepka <wojtek.kepka@protonmail.com>
2020-03-03 21:44:19 +0100

    ask for a path to conf and add example conf

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use rkd::config::*;
 use rkd::*;
+use std::env;
 
 fn main() {
     env_logger::init();
-    let c = Cfg::new("/tmp/temp_conf");
+    let mut args = env::args();
+    let cfg_file = args.skip(1).next().expect("Provide a path to config file");
+    let c = Cfg::new(cfg_file);
     let kb = c.parse().unwrap();
     run_rkd(kb);
 }
